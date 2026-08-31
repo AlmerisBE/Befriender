584fc46b19cb3e9ad32dc383f4d1a4f90a9eaf09
François Even <francois.even@almeris.net>
2026-08-31 13:44:20 +0200

    refactor(core): purge obsolete legacy action contracts and services
    - Delete the deprecated Befriender/Core/Actions directory including IFriendAction, IFriendActionService, and IRemoveFriendRequestService
    - Remove legacy action implementations and tests that were tightly coupled to the obsolete FriendProfile model
    - Confirm full reliance on the newly established ICharacterAction paradigm within the Characters feature
    
    Finalizes the cleanup of legacy action architecture following the successful migration to the Character aggregate root

--- Befriender/Core/Actions/Contracts/IFriendActionService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Befriender.Core.Actions.Contracts;
-
-using Befriender.Core.Friends.Models;
-using System.Collections.Generic;
-
-public interface IFriendActionService {
-    void RegisterAction(IFriendAction action);
-    IReadOnlyList<IFriendAction> GetAvailableActions(FriendProfile friend);
-}
